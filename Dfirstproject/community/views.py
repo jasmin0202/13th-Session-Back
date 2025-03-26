6fa45ba8300d3a5e0cad4ee1a10c6f89aa6d1ad4
gyk <ganada1375@gmail.com>
2025-03-26 23:54:06 +0900

    김가영: 03-26

---
+++ Dfirstproject/community/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from community.models import Post 
+
+# Create your views here.
+
+def List(request):
+    posts = Post.objects.filter(upload_time__lte = timezone.now()).order_by('upload_time') # posting 속 객체를 게시물 업로드 순서에 따라 배열
+    return render(request, 'list.html', {'posts' : posts}) # 화면에 보이게 렌더링
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) # 게시글을 업로드 할 때마다 매기는 번호
+    return render(request, 'detail.html', {'post':post})
