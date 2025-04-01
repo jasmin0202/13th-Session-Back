0142c1d6520b19c6bbb8d6a169382251c0413bdb
gyk <ganada1375@gmail.com>
2025-04-01 20:37:06 +0900

    blog 생성

--- newvenv/Scripts/activate.fish
+++ newvenv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\mycom\Documents\LIKELION_13th_study\13th-Session-Back\newvenv'
+set -gx VIRTUAL_ENV 'C:\Users\mycom\Documents\Projects\13th-Session-Back\newvenv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
